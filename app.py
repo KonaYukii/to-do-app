e5ba9416e4a2d92f5cf53d1718e18ccc78b0c19f
KonaYukii <amyli97@hotmail.com>
2021-07-18 17:04:35 -0400

    create and read method

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+tasklist = []
 
 @app.route("/", methods = ['GET'])
 def to_do():
   return "This application is a to do list. You can add, delete, and update things."
+
+@app.route("/add", methods = ['POST'])
+def add_task():
+  req = request.json
+  tasklist.append(req['content'])
+  return req
+
+@app.route("/tasklist", methods = ['GET'])
+def showlist():
+  return jsonify(tasklist)
